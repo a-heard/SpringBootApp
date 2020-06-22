529964279e70a9343430136692dbdd5c2a2fd7c0
a-heard <62103675+a-heard@users.noreply.github.com>
2020-06-22 02:24:28 -0400

    first app

---
+++ src/main/java/com/winspring/Student.java
@@ -0,0 +1,88 @@
+package com.winspring;
+
+
+
+
+import java.sql.Date;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int studentPin;
+    private String studentClass;
+    private Date birthday;
+    private String gender;
+    private String additional;
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getStudentPin() {
+        return studentPin;
+    }
+
+    public void setStudentPin(int studentPin) {
+        this.studentPin = studentPin;
+    }
+
+    public String getStudentClass() {
+        return studentClass;
+    }
+
+    public void setStudentClass(String studentClass) {
+        this.studentClass = studentClass;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAdditional() {
+        return additional;
+    }
+
+    public void setAdditional(String additional) {
+        this.additional = additional;
+    }
+
+    @Override
+    public String toString() {
+        return "User [ first name=" + firstName + ", last name=" +lastName + ", email=" + email + ", student pin # =" + studentPin + ", student class=" + studentClass + ", birthday=" + birthday + ",  gender=" + gender + ",Additional info=" + "]";
+    }
+    
+}
---
+++ src/main/java/com/winspring/demo/firstController.java
@@ -0,0 +1,30 @@
+package com.winspring.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class firstController {
+  @GetMapping("/enroll")
+  public String showForm(Model model){
+      User user = new User();
+      model.addAttribute("user",user);
+
+      List<String> studentClass = Arrays.asList("Undergrad", "Graduate");
+      model.addAttribute("studentClass", studentClass);
+      return "enrollmentForm";
+  }  
+//   @PostMapping("/enroll")
+//   public String submitForm(@ModelAttribute("user") User user){
+//       System.out.println(user);
+//       return "register_success";
+      
+//   }
+}
